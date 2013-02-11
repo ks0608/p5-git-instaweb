db825ae347f374309576a79d6dda9dcfcd06ff6c
ks <ks-for-git@live.jp>
2013-02-12 03:29:14 +0900

    initial commit

---
+++ lib/Git/instaweb.pm
@@ -0,0 +1,21 @@
+package Git::instaweb;
+use strict;
+use warnings;
+use utf8;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Git::instaweb - git instaweb on plack
+
+=head1 SYNOPSIS
+
+    % cd git-controlled-dir
+    % run-git-instaweb
+
+=cut
